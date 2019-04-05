1acc1e56b5434c00bbff04968e180e9a278ec164
mch1307 <mch1307@gmail.com>
2019-04-05 21:23:00 +0200

    update docker cli vers

--- go.mod
+++ go.mod
@@ -3,21 +3,27 @@ module github.com/bhuisgen/interlook
 go 1.12
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Microsoft/go-winio v0.4.12 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/engine-api v0.4.0
+	github.com/docker/docker v1.13.1
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.3.3 // indirect
 	github.com/fatih/structs v1.1.0
-	github.com/hashicorp/consul v1.4.3
-	github.com/hashicorp/go-cleanhttp v0.5.0 // indirect
-	github.com/hashicorp/go-rootcerts v1.0.0 // indirect
-	github.com/hashicorp/serf v0.8.2 // indirect
-	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/gorilla/mux v1.7.1 // indirect
+	github.com/hashicorp/consul/api v1.0.1
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/sirupsen/logrus v1.4.0
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	github.com/sirupsen/logrus v1.4.1
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	google.golang.org/grpc v1.19.1 // indirect
 	gopkg.in/yaml.v2 v2.2.2
+	gotest.tools v2.2.0+incompatible // indirect
 )
+
+replace github.com/docker/docker v1.13.1 => github.com/docker/engine v0.0.0-20190327083406-200b524eff60
--- provider/swarm/swarm.go
+++ provider/swarm/swarm.go
@@ -1,262 +1,227 @@
 package swarm
 
 import (
-    "sync"
-    "time"
+	"sync"
+	"time"
 
-    "github.com/bhuisgen/interlook/log"
-    "github.com/bhuisgen/interlook/service"
-    "github.com/docker/engine-api/client"
-    "github.com/docker/engine-api/types"
-    "github.com/docker/engine-api/types/filters"
-    "golang.org/x/net/context"
+	"github.com/bhuisgen/interlook/log"
+	"github.com/bhuisgen/interlook/service"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
 // Provider holds the provider configuration
 type Provider struct {
-    Name           string        `yaml:"name"`
-    Endpoint       string        `yaml:"endpoint"`
-    LabelSelector  []string      `yaml:"labelSelector"`
-    TLSCa          string        `yaml:"tlsCa"`
-    TLSCert        string        `yaml:"tlsCert"`
-    TLSKey         string        `yaml:"tlsKey"`
-    WatchInterval  time.Duration `yaml:"watchInterval"`
-    UpdateInterval time.Duration `yaml:"updateInterval"`
-    pollTicker     *time.Ticker
-    updateTicker   *time.Ticker
-    shutdown       chan bool
-    waitGroup      sync.WaitGroup
-    send           chan<- service.Message
+	Name           string        `yaml:"name"`
+	Endpoint       string        `yaml:"endpoint"`
+	LabelSelector  []string      `yaml:"labelSelector"`
+	TLSCa          string        `yaml:"tlsCa"`
+	TLSCert        string        `yaml:"tlsCert"`
+	TLSKey         string        `yaml:"tlsKey"`
+	WatchInterval  time.Duration `yaml:"watchInterval"`
+	UpdateInterval time.Duration `yaml:"updateInterval"`
+	pollTicker     *time.Ticker
+	updateTicker   *time.Ticker
+	shutdown       chan bool
+	waitGroup      sync.WaitGroup
+	send           chan<- service.Message
+	Filters        map[string][]string
+	services       []string
+	servicesLock   sync.RWMutex
+	cli            *client.Client
 }
 
 //type services map[string][]string
+func (p *Provider) initDockerCli() error {
+
+	p.cli = client.NewClientWithOpts(client.WithHTTPHeaders())
+	return nil
+}
 
 func (p *Provider) Start(receive <-chan service.Message, send chan<- service.Message) error {
 
-    p.shutdown = make(chan bool)
-    p.pollTicker = time.NewTicker(p.WatchInterval)
-    p.updateTicker = time.NewTicker(p.UpdateInterval)
-    p.send = send
-    p.waitGroup.Add(1)
+	p.shutdown = make(chan bool)
+	p.pollTicker = time.NewTicker(p.WatchInterval)
+	p.updateTicker = time.NewTicker(p.UpdateInterval)
+	p.send = send
+	p.waitGroup.Add(1)
 
-    for {
-        select {
-        case <-p.shutdown:
-            p.waitGroup.Done()
+	if err := p.initDockerCli(); err != nil {
+		return err
+	}
 
-            return
+	for {
+		select {
+		case <-p.shutdown:
+			p.waitGroup.Done()
 
-        case <-p.pollTicker.C:
-            log.Info("[DEBUG]", "new poll task")
-            p.poll()
+			return nil
 
-        case <-p.updateTicker.C:
-            log.Info("[DEBUG]", "new update task")
-            p.update()
-        }
-    }
-}
-    var msg service.Message
-    msg.Action = "add" // add, remove, update, check
-    msg.Service.Provider = "swarm"
-    msg.Service.Hosts = append(msg.Service.Hosts, "192.168.1.1")
-    msg.Service.Name = "test.swarm.com"
-    msg.Service.DNSAliases = []string{"test.swarm.com"}
-    msg.Service.Port = 8080
-    msg.Service.TLS = true
-    for {
-        time.Sleep(10 * time.Second)
-        send <- msg
-    }
+		case <-p.pollTicker.C:
+			log.Info("[DEBUG]", "new poll task")
+			p.poll()
 
+		case <-p.updateTicker.C:
+			log.Info("[DEBUG]", "new update task")
+			p.update()
+		}
+	}
 }
 
 func (p *Provider) Stop() error {
-    log.Info("Stopping Swarm provider")
-    p.shutdown <- true
-    return nil
+	log.Info("Stopping Swarm provider")
+	p.shutdown <- true
+	p.waitGroup.Wait()
+	return nil
 }
 
 // FIXME: will Init function initialize the Provider (from Provider)?
 type Provider0 struct {
-    WatchInterval  int
-    UpdateInterval int
-    Filters        map[string][]string
-    shutdown       chan struct{}
-    waitGroup      sync.WaitGroup
-    pollTicker     *time.Ticker
-    updateTicker   *time.Ticker
-    services       []string
-    servicesLock   sync.RWMutex
+	WatchInterval  int
+	UpdateInterval int
+
+	shutdown     chan struct{}
+	waitGroup    sync.WaitGroup
+	pollTicker   *time.Ticker
+	updateTicker *time.Ticker
+	services     []string
+	servicesLock sync.RWMutex
 }
 
-// FIXME: temp renamed for interface
-func (w *Provider) StartO() {
-    w.shutdown = make(chan struct{})
-    w.pollTicker = time.NewTicker(time.Duration(w.WatchInterval) * time.Second)
-    w.updateTicker = time.NewTicker(time.Duration(w.UpdateInterval) * time.Second)
+func (p *Provider) poll() {
+	ctx := context.Background()
 
-    log.Info("[DEBUG]", "provider started")
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Info("[ERROR]", err)
 
-    for {
-        select {
-        case <-w.shutdown:
-            w.waitGroup.Done()
+		return
+	}
 
-            return
+	servicesFilters := filters.NewArgs()
+	servicesFilters.Add("label", "interlook.host")
+	servicesFilters.Add("label", "interlook.port")
 
-        case <-w.pollTicker.C:
-            log.Info("[DEBUG]", "new poll task")
-            w.poll()
+	for name, values := range p.Filters {
+		for _, value := range values {
+			servicesFilters.Add(name, value)
+		}
+	}
 
-        case <-w.updateTicker.C:
-            log.Info("[DEBUG]", "new update task")
-            w.update()
-        }
-    }
+	data, err := cli.ServiceList(ctx, types.ServiceListOptions{
+		Filter: servicesFilters,
+	})
+	if err != nil {
+		log.Info("[ERROR]", "failed to list services", err)
+
+		return
+	}
+
+	var services []string
+
+	for _, service := range data {
+		services = append(services, service.ID)
+	}
+
+	p.servicesLock.Lock()
+	defer p.servicesLock.Unlock()
+
+	p.services = services
 }
 
-func (w *Provider) Stop() {
-    log.Info("[DEBUG]", "watcher stop request received")
+func (p *Provider) update() {
+	ctx := context.Background()
 
-    close(w.shutdown)
-    w.waitGroup.Wait()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Info("[ERROR]", err)
 
-    log.Info("[DEBUG]", "watcher stopped")
+		return
+	}
+
+	p.servicesLock.RLock()
+
+	for _, id := range p.services {
+		service, _, err := cli.ServiceInspectWithRaw(ctx, id)
+		if err != nil {
+			log.Info("[ERROR]", err)
+
+			return
+		}
+
+		tasksFilters := filters.NewArgs()
+		tasksFilters.Add("service", id)
+
+		tasks, err := cli.TaskList(ctx, types.TaskListOptions{
+			Filter: tasksFilters,
+		})
+		if err != nil {
+			log.Info("[ERROR]", err)
+
+			return
+		}
+
+		for _, task := range tasks {
+			id := task.Status.ContainerStatus.ContainerID
+
+			inspect, err := cli.ContainerInspect(ctx, id)
+			if err != nil {
+				log.Info("[ERROR]", err)
+
+				return
+			}
+
+			node, data, err := cli.NodeInspectWithRaw(ctx, task.NodeID)
+			if err != nil {
+				log.Info("[ERROR]", err)
+
+				return
+			}
+			ip := "1.2.3.4" // FIXME: no addr field ?
+			log.Info(node.ID)
+			log.Info(data)
+
+			var port string
+
+			if inspect.HostConfig.NetworkMode.IsHost() {
+				port = inspect.Config.Labels["interlook.port"]
+			} else {
+				exposed := false
+
+				for portMapping := range inspect.NetworkSettings.Ports {
+					if portMapping.Port() == port {
+						for _, target := range inspect.NetworkSettings.Ports[portMapping] {
+							if target.HostIP == "0.0.0.0" {
+								port = target.HostPort
+
+								exposed = true
+							}
+						}
+					}
+				}
+
+				if exposed == false {
+					continue
+				}
+			}
+
+			if inspect.State.Running && inspect.State.Health.Status != "healthy" {
+				p.addTarget(service.Spec.Labels["interlook.host"], ip, port, id)
+			} else {
+				p.removeTarget(service.Spec.Labels["interlook.host"], ip, port, id)
+			}
+		}
+	}
+
+	p.servicesLock.RUnlock()
 }
 
-func (w *Provider) poll() {
-    ctx := context.Background()
-
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        log.Info("[ERROR]", err)
-
-        return
-    }
-
-    servicesFilters := filters.NewArgs()
-    servicesFilters.Add("label", "interlook.host")
-    servicesFilters.Add("label", "interlook.port")
-
-    for name, values := range w.Filters {
-        for _, value := range values {
-            servicesFilters.Add(name, value)
-        }
-    }
-
-    data, err := cli.ServiceList(ctx, types.ServiceListOptions{
-        Filter: servicesFilters,
-    })
-    if err != nil {
-        log.Info("[ERROR]", "failed to list services", err)
-
-        return
-    }
-
-    var services []string
-
-    for _, service := range data {
-        services = append(services, service.ID)
-    }
-
-    w.servicesLock.Lock()
-    defer w.servicesLock.Unlock()
-
-    w.services = services
+func (p *Provider) addTarget(host string, ip string, port string, description string) {
+	// FIXME: update vhost
 }
 
-func (w *Provider) update() {
-    ctx := context.Background()
-
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        log.Info("[ERROR]", err)
-
-        return
-    }
-
-    w.servicesLock.RLock()
-
-    for _, id := range w.services {
-        service, _, err := cli.ServiceInspectWithRaw(ctx, id)
-        if err != nil {
-            log.Info("[ERROR]", err)
-
-            return
-        }
-
-        tasksFilters := filters.NewArgs()
-        tasksFilters.Add("service", id)
-
-        tasks, err := cli.TaskList(ctx, types.TaskListOptions{
-            Filter: tasksFilters,
-        })
-        if err != nil {
-            log.Info("[ERROR]", err)
-
-            return
-        }
-
-        for _, task := range tasks {
-            id := task.Status.ContainerStatus.ContainerID
-
-            inspect, err := cli.ContainerInspect(ctx, id)
-            if err != nil {
-                log.Info("[ERROR]", err)
-
-                return
-            }
-
-            node, data, err := cli.NodeInspectWithRaw(ctx, task.NodeID)
-            if err != nil {
-                log.Info("[ERROR]", err)
-
-                return
-            }
-            ip := "1.2.3.4" // FIXME: no addr field ?
-            log.Info(node.ID)
-            log.Info(data)
-
-            var port string
-
-            if inspect.HostConfig.NetworkMode.IsHost() {
-                port = inspect.Config.Labels["interlook.port"]
-            } else {
-                exposed := false
-
-                for portMapping := range inspect.NetworkSettings.Ports {
-                    if portMapping.Port() == port {
-                        for _, target := range inspect.NetworkSettings.Ports[portMapping] {
-                            if target.HostIP == "0.0.0.0" {
-                                port = target.HostPort
-
-                                exposed = true
-                            }
-                        }
-                    }
-                }
-
-                if exposed == false {
-                    continue
-                }
-            }
-
-            if inspect.State.Running && inspect.State.Health.Status != "healthy" {
-                w.addTarget(service.Spec.Labels["interlook.host"], ip, port, id)
-            } else {
-                w.removeTarget(service.Spec.Labels["interlook.host"], ip, port, id)
-            }
-        }
-    }
-
-    w.servicesLock.RUnlock()
-}
-
-func (w *Provider) addTarget(host string, ip string, port string, description string) {
-    // FIXME: update vhost
-}
-
-func (w *Provider) removeTarget(host string, ip string, port string, description string) {
-    // FIXME: update vhost
+func (p *Provider) removeTarget(host string, ip string, port string, description string) {
+	// FIXME: update vhost
 }
