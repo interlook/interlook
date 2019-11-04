d88549f595c8f9ddd5db98e105a5d4093864c599
mch1307 <mch1307@gmail.com>
2019-11-04 22:53:33 +0100

    remove genRefConfigFile func

--- config/config.go
+++ config/config.go
@@ -1,23 +1,16 @@
 package config
 
 import (
-	"fmt"
-	"github.com/interlook/interlook/log"
 	"github.com/interlook/interlook/provisioner/dns/consul"
 	"github.com/interlook/interlook/provisioner/loadbalancer/f5ltm"
 	"github.com/interlook/interlook/provisioner/loadbalancer/kemplm"
 	"io/ioutil"
-	"os"
 	"time"
 
 	"github.com/interlook/interlook/provider/kubernetes"
 	"github.com/interlook/interlook/provider/swarm"
 	"github.com/interlook/interlook/provisioner/ipam/ipalloc"
 	"gopkg.in/yaml.v3"
-)
-
-const (
-	refConfigFile = "./docs/interlook.yml"
 )
 
 // ServerConfiguration holds the configuration
@@ -53,7 +46,6 @@ type ServerConfiguration struct {
 
 // ReadConfig parse the configuration
 func ReadConfig(filename string) (*ServerConfiguration, error) {
-	genReferenceConfigYAMLFile()
 	var cfg ServerConfiguration
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -64,38 +56,4 @@ func ReadConfig(filename string) (*ServerConfiguration, error) {
 		return &cfg, err
 	}
 	return &cfg, nil
-}
-
-func genReferenceConfigYAMLFile() {
-
-	cfg := ServerConfiguration{}
-	cfg.Provider.Swarm = &swarm.Provider{}
-	cfg.Provider.Kubernetes = &kubernetes.Extension{}
-	cfg.IPAM.IPAlloc = &ipalloc.IPAlloc{}
-	cfg.DNS.Consul = &consul.Consul{}
-	cfg.LB.KempLM = &kemplm.KempLM{}
-	cfg.LB.F5LTM = &f5ltm.BigIP{}
-
-	refFile, err := os.OpenFile(refConfigFile, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Printf("Error opening file %v %v", refConfigFile, err)
-	}
-
-	if err := refFile.Truncate(0); err != nil {
-		log.Error(err)
-	}
-	_, err = refFile.Seek(0, 0)
-	if err != nil {
-		log.Error(err)
-	}
-
-	defer func() {
-		if err := refFile.Close(); err != nil {
-			fmt.Printf("Error closing filename %v", err)
-		}
-	}()
-
-	data, _ := yaml.Marshal(cfg)
-	_, _ = refFile.Write(data)
-
 }
